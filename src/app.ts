c3568f742fc692c68540bf65bb91d291479063e6
doremilan <yooossi@gmail.com>
2022-09-18 01:16:47 +0900

    fix: session 코드삭제

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import { connect } from "./models/index";
-// import passport from "passport";
-import session from "express-session";
+// import session from "express-session";
 import passportConfig from "./middlewares/passport";
 import router from "./routers/index";
 import { config } from "./config/constants";
@@ -23,17 +22,17 @@ app.use(helmet());
 
 app.use(router);
 
-app.use(
-  session({
-    secret: config.sessionSecret,
-    resave: true,
-    saveUninitialized: true,
-    // cookie: {
-    //   httpOnly: true,
-    //   secure: false,
-    // },
-  })
-);
+// app.use(
+//   session({
+//     secret: config.sessionSecret,
+//     resave: true,
+//     saveUninitialized: true,
+//     // cookie: {
+//     //   httpOnly: true,
+//     //   secure: false,
+//     // },
+//   })
+// );
 
 // app.use(passport.initialize());
 // app.use(passport.session());
