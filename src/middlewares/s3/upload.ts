cefb8d7e4ecb581d1500cb51eaa53c8b06ccc9fe
doremilan <yooossi@gmail.com>
2022-10-22 16:57:04 +0900

    fix: 이미지 업로더 추가

--- src/middlewares/s3/upload.ts
+++ src/middlewares/s3/upload.ts
@@ -1,52 +1,74 @@
-// import AWS from "aws-sdk";
-// import multer, { FileFilterCallback } from "multer";
-// import multerS3 from "multer-s3";
-// import path from "path";
-// import dayjs from "dayjs";
-// import { Request } from "express";
-// import { ErrorMessageEnum } from "../../common/type";
-// import { s3Bucket } from "../../configs/constants";
+import AWS from "aws-sdk";
+import multer, { FileFilterCallback } from "multer";
+import multerS3 from "multer-s3";
+import path from "path";
+import dayjs from "dayjs";
+import { Request } from "express";
+import { ErrorMessageEnum } from "../../common/type";
+import { s3Bucket } from "../../configs/constants";
 
-// type FileNameCallback = (error: Error | null, filename: string) => void;
+type FileNameCallback = (error: Error | null, filename: string) => void;
 
-// AWS.config.update({
-//   accessKeyId: s3Bucket.accesskey,
-//   secretAccessKey: s3Bucket.secretAcesskey,
-//   region: s3Bucket.region,
-// });
+AWS.config.update({
+  accessKeyId: s3Bucket.accesskey,
+  secretAccessKey: s3Bucket.secretAcesskey,
+  region: s3Bucket.region,
+});
 
-// const s3 = new AWS.S3();
+const s3 = new AWS.S3();
 
-// const fileFilter = (
-//   req: Request,
-//   file: Express.MulterS3.File,
-//   cb: FileFilterCallback
-// ) => {
-//   if (
-//     file.mimetype === "image/png" ||
-//     file.mimetype === "image/jpg" ||
-//     file.mimetype === "image/jpeg"
-//   ) {
-//     cb(null, true);
-//   } else {
-//     return cb(new Error(ErrorMessageEnum.WRONG_EXTENSION));
-//   }
-// };
+const fileFilter = (
+  req: Request,
+  file: Express.MulterS3.File,
+  cb: FileFilterCallback
+) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    return cb(new Error(ErrorMessageEnum.WRONG_EXTENSION));
+  }
+};
 
-// export const imageUploader = multer({
+export const imageUploader = multer({
+  storage: multerS3({
+    s3: <any>s3,
+    bucket: s3Bucket.bucket,
+    acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (
+      req: Request,
+      file: Express.MulterS3.File,
+      cb: FileNameCallback
+    ) {
+      cb(
+        null,
+        `stampImage/${dayjs().format("YYYY/MM/DD")}_${path.basename(
+          file.originalname
+        )}`
+      );
+    },
+  }),
+  fileFilter,
+});
+
+// export const multiImageUploader = multer({
 //   storage: multerS3({
 //     s3: <any>s3,
 //     bucket: s3Bucket.bucket,
 //     acl: "public-read",
 //     contentType: multerS3.AUTO_CONTENT_TYPE,
 //     key: function (
-//       req: Request,
+//       req: Express.Request,
 //       file: Express.MulterS3.File,
 //       cb: FileNameCallback
 //     ) {
 //       cb(
 //         null,
-//         `stampImage/${dayjs().format("YYYY/MM/DD")}_${path.basename(
+//         `reveiwImage/${dayjs().format("YYYY/MM/DD")}_${path.basename(
 //           file.originalname
 //         )}`
 //       );
@@ -54,25 +76,3 @@
 //   }),
 //   fileFilter: fileFilter,
 // });
-
-// // export const multiImageUploader = multer({
-// //   storage: multerS3({
-// //     s3: <any>s3,
-// //     bucket: s3Bucket.bucket,
-// //     acl: "public-read",
-// //     contentType: multerS3.AUTO_CONTENT_TYPE,
-// //     key: function (
-// //       req: Express.Request,
-// //       file: Express.MulterS3.File,
-// //       cb: FileNameCallback
-// //     ) {
-// //       cb(
-// //         null,
-// //         `reveiwImage/${dayjs().format("YYYY/MM/DD")}_${path.basename(
-// //           file.originalname
-// //         )}`
-// //       );
-// //     },
-// //   }),
-// //   fileFilter: fileFilter,
-// // });
--- src/routers/stamp-router.ts
+++ src/routers/stamp-router.ts
@@ -2,11 +2,9 @@ import { Router } from "express";
 const router = Router();
 import authMiddleware from "../middlewares/auth-middleware/auth-middleware";
 import { findAllStamps, certifyStamp } from "../controllers/stamp-controller";
-// import { imageUploader } from "../middlewares/s3/upload";
+import { imageUploader } from "../middlewares/s3/upload";
 
 router.get("/", authMiddleware, findAllStamps);
-router.put("/:stampId", certifyStamp);
+router.put("/:stampId", imageUploader.single("stampImage"), certifyStamp);
 
 export default router;
-
-// imageUploader.single("stampImage"),
