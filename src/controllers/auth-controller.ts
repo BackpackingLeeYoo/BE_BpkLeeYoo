7902af9b19aa47469f3f588599c5d7a1ac8a1eee
doremilan <yooossi@gmail.com>
2022-10-30 19:37:50 +0900

    fix: 콘솔 제거

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -6,7 +6,6 @@ import { ErrorMessageEnum, StatusCodeEnum, UserParams } from "../common/type";
 import { getUserById } from "../services/auth-services";
 
 const kakaoCallback = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req);
   passport.authenticate(
     "kakao",
     { failureRedirect: "/" },
