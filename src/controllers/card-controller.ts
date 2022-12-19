217695daa0a759ea69840840d827d28c4290b2ff
doremilan <yooossi@gmail.com>
2022-12-19 23:58:06 +0900

    fix: 오타수정

--- src/controllers/card-controller.ts
+++ src/controllers/card-controller.ts
@@ -3,13 +3,13 @@ import Card from "../schemas/card-model";
 
 const findCard = async (req: Request, res: Response) => {
   try {
-    const { cardId } = req.params
+    const { cardId } = req.params;
 
-    const existCard = await Card.findOne({ id: cardId })
-    
+    const existCard = await Card.findOne({ _id: cardId });
+
     if (!existCard) {
       return res.status(400).send({
-        message: '해당하는 카드를 찾을 수 없습니다.',
+        message: "해당하는 카드를 찾을 수 없습니다.",
       });
     }
 
@@ -17,8 +17,8 @@ const findCard = async (req: Request, res: Response) => {
       cardId: existCard._id,
       to: existCard.to,
       content: existCard.content,
-      from: existCard.from
-    }
+      from: existCard.from,
+    };
 
     res.status(200).json({
       card,
@@ -26,23 +26,23 @@ const findCard = async (req: Request, res: Response) => {
   } catch (err) {
     console.error(err);
     res.status(400).send({
-      message: '카드 조회 실패',
+      message: "카드 조회 실패",
     });
   }
 };
 
 const createCard = async (req: Request, res: Response) => {
   try {
-    const { to, content, from } = req.body
+    const { to, content, from } = req.body;
 
-    const newCard = await Card.create({ to, content, from })
-    
+    const newCard = await Card.create({ to, content, from });
+
     const card = {
       cardId: newCard._id,
       to: newCard.to,
       content: newCard.content,
-      from: newCard.from
-    }
+      from: newCard.from,
+    };
 
     res.status(200).json({
       card,
@@ -50,7 +50,7 @@ const createCard = async (req: Request, res: Response) => {
   } catch (err) {
     console.error(err);
     res.status(400).send({
-      message: '카드 작성 실패',
+      message: "카드 작성 실패",
     });
   }
 };
